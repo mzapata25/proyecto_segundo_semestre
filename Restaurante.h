56bd8903adefaf7e0aa6febd67e6d2dd56b339be
mzapata25 <142268986+mzapata25@users.noreply.github.com>
2024-06-10 01:06:30 -0600

    Se añade archivo .h de la clase Restaurante

---
+++ Restaurante.h
@@ -0,0 +1,45 @@
+#ifndef RESTAURANTE_H_
+#define RESTAURANTE_H_
+
+#include <string>
+
+#include <sstream>
+
+using namespace std;
+
+class Restaurante: public Negocio{
+    private:
+        string tipo_restaurante{};
+        string licencia_alcohol{};
+    public:
+        Restaurante(): Negocio(){};
+        Restaurante(string n, string h_a, string h_c, string d_a, int pl, string pr, string tip_rest, string l_a): Negocio(n, h_a, h_c, d_a, pl, pr), tipo_restaurante(tip_rest), licencia_alcohol(l_a){};
+		void setTipRest(string nuevoTipRest);
+		string getTipRest()const;
+		void setLicencia(string nuevaLicencia);
+		string getLicencia()const;
+		string to_string();
+};
+
+void Restaurante::setTipRest(string nuevoTipRest){
+	tipo_restaurante = nuevoTipRest;
+}
+
+string Restaurante::getTipRest()const{
+	return tipo_restaurante;
+}
+
+void Restaurante::setLicencia(string nuevaLicencia){
+	licencia_alcohol = nuevaLicencia;
+}
+
+string Restaurante::getLicencia()const{
+	return licencia_alcohol;
+}
+
+string Restaurante::to_string(){
+	stringstream aux;
+	aux << "Nombre del restaurante: " << nombre << "\nHora de apertura: " << hora_apertura << "\nHora de cierre: " << hora_cierre << "\nDias de apertura: " << dias_apertura << "\nUbicado en la planta: " << planta << "\nPropietario: " << propietario << "\nTipo de restaurante: " << tipo_restaurante << "\nVenta de alcohol: "<< licencia_alcohol <<"\n";
+	return aux.str();
+}
+#endif
