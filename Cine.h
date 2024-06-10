4b2559196358995d13665ffbf5761b65034f53ba
mzapata25 <142268986+mzapata25@users.noreply.github.com>
2024-06-10 01:03:16 -0600

    Se añade archivo .h de la clase Cine

---
+++ Cine.h
@@ -0,0 +1,35 @@
+#ifndef CINE_H_
+#define CINE_H_
+
+#include <string>
+
+#include <sstream>
+
+using namespace std;
+
+class Cine: public Negocio{
+    private:
+        int numero_salas{};
+    public:
+        Cine(): Negocio(){};
+        Cine(string n, string h_a, string h_c, string d_a, int pl, string pr, int n_s): Negocio(n, h_a, h_c, d_a, pl, pr), numero_salas(n_s){};
+		void setNumSalas(int nuevasSalas);
+		int getNumSalas() const;
+		string to_string();
+};
+
+void Cine::setNumSalas(int nuevasSalas){
+	numero_salas = nuevasSalas;
+}
+
+int Cine::getNumSalas()const{
+	return numero_salas;
+}
+
+string Cine::to_string(){
+	stringstream aux;
+	aux << "Nombre del cine: " << nombre << "\nHora de apertura: " << hora_apertura << "\nHora de cierre: " << hora_cierre << "\nDias de apertura: " << dias_apertura << "\nUbicado en la planta: " << planta << "\nPropietario: " << propietario << "\nNúmero de salas: " << numero_salas << "\n";
+	return aux.str();
+}
+
+#endif
