d5e4401c1c973ea4c648031e80bb64d4315d976e
mzapata25 <142268986+mzapata25@users.noreply.github.com>
2024-06-10 01:07:23 -0600

    Se añade archivo .h de la clase Tienda_ropa

---
+++ Tiendaropa.h
@@ -0,0 +1,34 @@
+#ifndef TIENDAROPA_H_
+#define TIENDAROPA_H_
+
+#include <string>
+
+#include <sstream>
+
+using namespace std;
+
+class Tienda_ropa: public Negocio{
+    private:
+        string tipo_ropa{};
+    public:
+        Tienda_ropa(): Negocio(){};
+        Tienda_ropa(string n, string h_a, string h_c, string d_a, int pl, string pr, string t_r): Negocio(n, h_a, h_c, d_a, pl, pr), tipo_ropa(t_r){};
+		void setTipoR(string nuevoTipoR);
+		string getTipoR() const;
+		string to_string();
+};
+
+void Tienda_ropa::setTipoR(string nuevoTipoR){
+	tipo_ropa = nuevoTipoR;
+}
+
+string Tienda_ropa::getTipoR()const{
+	return tipo_ropa;
+}
+
+string Tienda_ropa::to_string(){
+	stringstream aux;
+	aux << "Nombre de la tienda: " << nombre << "\nHora de apertura: " << hora_apertura << "\nHora de cierre: " << hora_cierre << "\nDias de apertura: " << dias_apertura << "\nUbicado en la planta: " << planta << "\nPropietario: " << propietario << "\nTipo de ropa: " << tipo_ropa << "\n";
+	return aux.str();
+}
+#endif
