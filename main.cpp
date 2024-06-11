75153479faf078a77d3c96dfdf5efd80204d5f2e
mzapata25 <142268986+mzapata25@users.noreply.github.com>
2024-06-11 09:10:13 -0600

    Actualización final del código del main

--- main.cpp
+++ main.cpp
@@ -9,13 +9,20 @@ using namespace std;
 int main()
 {
 	Centro_comercial CC;
-	CC.crea_tienda();
+    	CC.crea_tienda();
 	CC.muestra_tienda();
 	cout << endl;
+	CC.crea_tienda_usuario();
+	CC.muestra_tienda();
 	CC.crea_restaurante();
 	CC.muestra_restaurante();
 	cout << endl;
+	CC.crea_restaurante_usuario();
+	CC.muestra_restaurante();
 	CC.crea_cine();
+	CC.muestra_cine();
+	cout << endl;
+	CC.crea_cine_usuario();
 	CC.muestra_cine();
     return 0;
 }
