37e0198ee374116ef016097c15280ecf20099f52
mzapata25 <142268986+mzapata25@users.noreply.github.com>
2024-06-13 01:02:05 -0600

    Comentarios en Centrocomercial.h

--- Centrocomercial.h
+++ Centrocomercial.h
@@ -1,7 +1,17 @@
+/*
+ * Proyecto Segundo Semestre clase Centro_comercial
+ * A01708573
+ * 13-06-2024
+ *
+ * Esta clase define objetos tipo Centro_comercial y contiene las funciones para crear y mostrar diferentes negocios.
+ */
+
+
+
 #ifndef CENTROCOMERCIAL_H_
 #define CENTROCOMERCIAL_H_
 
-#include "Negocio.h"
+#include "Negocio.h" //Bibliotecas para usar sus objetos aquí.
 #include "Tiendaropa.h"
 #include "Restaurante.h"
 #include "Cine.h"
@@ -15,34 +25,61 @@ using namespace std;
 const int MAXN = 100; // Representa el máximo número de negocios, puede haber menos de 100.
 
 class Centro_comercial{
+//Declaro variables privadas
     private:
-        Tienda_ropa * tiendas[MAXN];
+        Tienda_ropa * tiendas[MAXN]; //Se definen apuntadores para usar polimorfismo.
         Restaurante * restaurantes[MAXN];
         Cine * cines[MAXN];
-		int tiendas_totales{};
-		int restaurantes_totales{};
-		int cines_totales{};
+	    int tiendas_totales{};
+	    int restaurantes_totales{};
+	    int cines_totales{};
+//Declaro funciones públicas
     public:
         Centro_comercial(): tiendas_totales(0), restaurantes_totales(0), cines_totales(0){};
         void crea_tienda();
         void crea_restaurante();
         void crea_cine();
-		void muestra_tienda();
-		void muestra_restaurante();
-		void muestra_cine();
-		void crea_tienda_usuario();
-		void crea_restaurante_usuario();
-		void crea_cine_usuario();
+	    void muestra_tienda();
+	    void muestra_restaurante();
+	    void muestra_cine();
+	    void crea_tienda_usuario();
+	    void crea_restaurante_usuario();
+	    void crea_cine_usuario();
 };
 
+/*
+ * crea_tienda genera objetos en tiendas[]
+ *
+ * genera objetos de tipo Tienda_ropa y los
+ * guarda en la varibale de instancia en tiendas[] (arreglo de tiendas)
+ * Solo crea un único objeto a modo de demostración.
+ * @param
+ * @return
+ */
+
 void Centro_comercial::crea_tienda(){
+	//new crea el objeto en tiempo de ejecución para usar polimorfismo
 	tiendas[tiendas_totales] = new Tienda_ropa("Emporio Deportivo", "09:00", "20:00", "Lunes a viernes", 1, "Mateo Zapata", "Deportiva");
 	tiendas_totales++;
 }
+
+/*
+ * muestra_tienda imprime tiendas
+ *
+ * utiliza el arreglo tiendas[] y el número de tiendas totales, para recorrer todo el
+ * arreglo imprimiendo cada uno de los objetos con su método to_string().
+ *
+ * @param
+ * @return
+ */
+
 void Centro_comercial::muestra_tienda() {
+   //Ciclo que recorre el arreglo e imprime cada objeto.
   for (int i = 0; i < tiendas_totales; i++)
-    cout << tiendas[i] -> to_string();
+  cout << tiendas[i] -> to_string();
 }
+
+// Las mismas funciones para crear y mostrar son usadas con los tres tipos de objetos (Tienda_ropa, Restaurante y Cine).
 
 void Centro_comercial::crea_restaurante(){
 	restaurantes[restaurantes_totales] = new Restaurante("Il Ristorante", "07:00", "23:30", "Todos los días", 2, "Mateo Zapata", "Italiano", "Sí");
@@ -64,6 +101,19 @@ void Centro_comercial::muestra_cine(){
 	  cout << cines[i] -> to_string();
 }
 
+
+/*
+ * crea_tienda_usuario genera objetos creados por el usuario en tiendas[]
+ *
+ * Pide datos al usuario para poder
+ * generar objetos de tipo Tienda_ropa y los
+ * guarda en la varibale de instancia en tiendas[] (arreglo de tiendas)
+ * Crea tantos objetos como le pida el usuario, siempre que no exceda MAXN.
+ * @param
+ * @return
+ */
+
+
 void Centro_comercial::crea_tienda_usuario(){
 	int desea_crear;
 	int cantidad_crear;
@@ -76,6 +126,7 @@ void Centro_comercial::crea_tienda_usuario(){
 	string ropa_crear;
 	cout << "¿Desea crear alguna tienda de ropa?" << endl << "1. Sí" << endl << "2. No" << endl;
 	cin >> desea_crear;
+    // Ciclo para evitar que el programa se cierre sin que el usuario decida una de las dos opciones,
 	while (desea_crear != 1 and desea_crear != 2){
 		cout << "Error. Solo puede responder con el número 1 (sí) o el numero 2 (no)" << endl;
 		cin >> desea_crear;
@@ -108,6 +159,8 @@ void Centro_comercial::crea_tienda_usuario(){
 		}
 	}
 }
+
+// Se utilizan las mismas funciones para que el usuario genere restaurantes y cines.
 
 void Centro_comercial::crea_restaurante_usuario(){
 	int desea_crear;
