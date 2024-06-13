a414994e457b8ae21b7045ecd14cdaad637abd1e
mzapata25 <142268986+mzapata25@users.noreply.github.com>
2024-06-13 01:24:48 -0600

    Comentarios en Restaurante.h

--- Restaurante.h
+++ Restaurante.h
@@ -8,17 +8,19 @@
 using namespace std;
 
 class Restaurante: public Negocio{
-    private:
+   //Declaro variables privadas
+   private:
         string tipo_restaurante{};
         string licencia_alcohol{};
+    //Declaro métodos públicos
     public:
         Restaurante(): Negocio(){};
         Restaurante(string n, string h_a, string h_c, string d_a, int pl, string pr, string tip_rest, string l_a): Negocio(n, h_a, h_c, d_a, pl, pr), tipo_restaurante(tip_rest), licencia_alcohol(l_a){};
-		void setTipRest(string nuevoTipRest);
-		string getTipRest()const;
-		void setLicencia(string nuevaLicencia);
-		string getLicencia()const;
-		string to_string();
+	void setTipRest(string nuevoTipRest);
+	string getTipRest()const;
+	void setLicencia(string nuevaLicencia);
+	string getLicencia()const;
+	string to_string();
 };
 
 void Restaurante::setTipRest(string nuevoTipRest){
@@ -36,6 +38,15 @@ void Restaurante::setLicencia(string nuevaLicencia){
 string Restaurante::getLicencia()const{
 	return licencia_alcohol;
 }
+
+/**
+ * to_string convierte a atributos a string.
+ *
+ * concatena todos los valores de los atributos en un string para ser impreso
+ *
+ * @param
+ * @return string con los valores y texto concatenado.
+ */
 
 string Restaurante::to_string(){
 	stringstream aux;
